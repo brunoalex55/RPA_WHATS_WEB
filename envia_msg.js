435e8d54420dcc1fcb7b6ba3786e168b7441916a
brunoalexandrecj <brunoalexandrecj@gmail.com>
2022-09-09 12:14:43 -0300

    adjust

--- envia_msg.js
+++ envia_msg.js
@@ -2,8 +2,19 @@ const { Client, Location, List, Buttons, LocalAuth} = require('./index');
 
 const client = new Client({ puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']} });
 
-client.initialize();
-
-client.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
 });
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
