3513b57202151444b8920e6d67b9e7b4afa1fa52
brunoalexandrecj <brunoalexandrecj@gmail.com>
2022-09-09 12:13:29 -0300

    as

--- envia_msg.js
+++ envia_msg.js
@@ -2,26 +2,8 @@ const { Client, Location, List, Buttons, LocalAuth} = require('./index');
 
 const client = new Client({ puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']} });
 
+client.initialize();
 
 client.on('loading_screen', (percent, message) => {
     console.log('LOADING SCREEN', percent, message);
-}); 
-
-client.on('qr', (qr) => {
-    // NOTE: This event will not be fired if a session is specified.
-    console.log('QR RECEIVED', qr);
 });
-
-client.on('authenticated', () => {
-    console.log('AUTHENTICATED');
-});
-
-client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
-    console.error('AUTHENTICATION FAILURE', msg);
-});
-
-client.on('ready', () => {
-    console.log('READY');
-});
-client.initialize();
