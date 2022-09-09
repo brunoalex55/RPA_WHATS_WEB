ef1ad5edd8a42455a9d42af42847280ad74ffd6c
brunoalexandrecj <brunoalexandrecj@gmail.com>
2022-09-09 12:07:12 -0300

    adjustment

--- example.js
+++ example.js
@@ -2,7 +2,8 @@ const { Client, Location, List, Buttons, LocalAuth} = require('./index');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true }
+    puppeteer: { headless: true },
+    args: ['--no-sandbox']
 });
 
 client.initialize();
